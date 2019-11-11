25f662bc876c3c5f912f299d955f0e2c25598a87
Chris Foteinos <chrisfot7@gmail.com>
2019-11-11 21:52:43 +0200

    Improved smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var intro = document.getElementById('introduction-text');
-var arrowDown = document.getElementById('arrow-down');
-var arrowUp = document.getElementById('arrow-up');
+const intro = document.getElementById('introduction-text');
+const arrowDown = document.getElementById('arrow-down');
+const arrowUp = document.getElementById('arrow-up');
 
 window.addEventListener('scroll', function() {
     if (isAnyPartOfElementInViewport(intro)) {
@@ -12,6 +12,23 @@ window.addEventListener('scroll', function() {
     }
 });
 
+arrowDown.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetElement = document.getElementById('about-me');
+    smoothScrollToElement(targetElement);
+});
+
+arrowUp.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetElement = document.getElementById('top');
+    smoothScrollToElement(targetElement);
+});
+
+/**
+ * Finds out if any part of the target element is in display.
+ * @param el
+ * @returns {boolean}
+ */
 function isAnyPartOfElementInViewport(el) {
     const rect = el.getBoundingClientRect();
     const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
@@ -19,4 +36,17 @@ function isAnyPartOfElementInViewport(el) {
     const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
     const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
     return (vertInView && horInView);
+}
+
+/**
+ * Scroll to the given element smoothly.
+ * @param el
+ */
+function smoothScrollToElement(el) {
+    const rect = el.getBoundingClientRect();
+    window.scrollTo({
+        top: rect.top,
+        left: rect.left,
+        behavior: 'smooth'
+    });
 }
